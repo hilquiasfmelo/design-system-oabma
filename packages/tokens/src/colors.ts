1fd44945b99adbac1ffd2740b8102047736cc920
hilquiasfmelo <hilquiasfmelo@hotmail.com>
2023-01-18 19:50:19 -0300

    🚀 Configuração dos pacotes completo

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+  white: '#FFFFFF',
+
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  blue300: '#076ABD',
+  blue500: '#065597',
+  blue700: '#054071',
+
+  red300: '#AC1E1C',
+  red500: '#8A1817',
+  red700: '#691211',
+}
